0d4184981b79b51430d59804a811c5aff42fa0e2
Choco Le <lethitan2412001@gmail.com>
2024-09-14 12:25:30 +0700

    update CORS - allow all

--- src/main/java/com/mgnt/warehouse/controller/UserController.java
+++ src/main/java/com/mgnt/warehouse/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.mgnt.warehouse.controller;
 
+import com.mgnt.warehouse.modal.auth.User;
+import com.mgnt.warehouse.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("http://localhost:5173")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserRepository userRepository;
+
     @GetMapping("/list")
-    public String getUsers() {
-        return "success";
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
